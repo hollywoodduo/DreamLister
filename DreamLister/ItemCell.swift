af16cc21d3779757f37b78db99b8d9252095a8e1
hollywoodduo <matthew.perkins@gmail.com>
2017-01-19 12:26:03 -0500

    added update insert delete move

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Matthew J. Perkins on 1/18/17.
+//  Copyright © 2017 Matthew J. Perkins. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+}
